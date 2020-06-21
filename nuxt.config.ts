393ad5a260c1bea9331ac63304d1c575024da8c6
iemong <kiyoshidainagon@gmail.com>
2020-06-21 23:03:11 +0900

    meta追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 export default {
+  head: {
+    titleTemplate: 'どこでもドラ',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Meta description' },
+      { hid: 'keyword', name: 'keyword', content: 'timer' }
+    ]
+  },
   buildModules: ['@nuxt/typescript-build', '@nuxtjs/tailwindcss'],
   plugins: ['~/plugins/composition-api']
 }
